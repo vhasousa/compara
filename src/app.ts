6b71128dd616833672a41ee60e1872be67e3cb48
vhasousa <vitorhugo_as@hotmail.com>
2023-03-01 10:13:20 -0300

    Add cors to api call

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,11 @@ import swaggerUi from 'swagger-ui-express';
 
 import { router } from "./routes";
 import swaggerFile from   "./swagger.json";
-// import cors from 'cors';
+import cors from 'cors';
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 
 app.use(express.json());
 
